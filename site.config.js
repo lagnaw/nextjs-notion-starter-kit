a5fbd59b477f18e3aea6cfe82fabcd8ed568e2f2
Nawfal Laghzali <88565117+lagnaw@users.noreply.github.com>
2021-12-08 02:24:52 +0100

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '447bd6e4ba2245be9d404e15fc764ffd',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
+  name: 'Vercel Notion Blog ',
   domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'NL',
 
   // open graph metadata (optional)
   description: 'Example site description',
